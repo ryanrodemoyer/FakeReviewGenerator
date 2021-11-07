a23253bc97a76af3e3a221c0b53159cfb84ae7bd
Ryan Rodemoyer <ryan.rodemoyer@icloud.com>
2021-11-07 13:00:18 -0500

    tabs 4 spaces

--- source/markov/Markov.cs
+++ source/markov/Markov.cs
@@ -8,151 +8,152 @@ using DataStructures.RandomSelector;
 
 namespace markov
 {
-    public class Markov
-    {
-        public Dictionary<string, Result> Model { get; }
+	public class Markov
+	{
+		public Dictionary<string, Result> Model { get; }
 
-        public Markov(Dictionary<string, Result> model)
-        {
-            Model = model;
-        }
+		public Markov(Dictionary<string, Result> model)
+		{
+			Model = model;
+		}
 
-        public IEnumerable<string> GetNextWord()
-        {
-            bool first = true;
+		public IEnumerable<string> GetNextWord()
+		{
+			bool first = true;
 
-            string word = null;
-            if (first)
-            {
-                var selec = new DynamicRandomSelector<string>();
+			string word = null;
+			if (first)
+			{
+				var selec = new DynamicRandomSelector<string>();
 
-                var startingWords = Model.Values.Where(x => x.IsStartingWord);
-                int totalCount = startingWords.Sum(x => x.Counter);
-                foreach (var record in startingWords)
-                {
-                    float raw = (float)record.Counter / (float)totalCount;
+				var startingWords = Model.Values.Where(x => x.IsStartingWord);
+				int totalCount = startingWords.Sum(x => x.Counter);
+				foreach (var record in startingWords)
+				{
+					float raw = (float)record.Counter / (float)totalCount;
 
-                    selec.Add(record.Value, raw);
-                }
+					selec.Add(record.Value, raw);
+				}
 
-                selec.Build();
+				selec.Build();
 
-                word = selec.SelectRandomItem();
-                yield return Model[word].Random;
-            }
+				word = selec.SelectRandomItem();
+				yield return Model[word].Random;
+			}
 
-            while (true)
-            {
-                word = Model[word].Random;
-                yield return word;
-            }
-        }
-    }
+			while (true)
+			{
+				word = Model[word].Random;
+				yield return word;
+			}
+		}
+	}
 
-    public class MarkovConfig
-    {
-        public Func<List<string>> Tokenizer { get; set; }
+	public class MarkovConfig
+	{
+		public Func<List<string>> Tokenizer { get; set; }
 
-        public MarkovConfig(Func<List<string>> tokenizer)
-        {
-            Tokenizer = tokenizer;
-        }
-    }
+		public MarkovConfig(Func<List<string>> tokenizer)
+		{
+			Tokenizer = tokenizer;
+		}
+	}
 
-    public static class MarkovBuilder {
-        public static Markov Build(MarkovConfig config)
-        {
-            List<string> tokens = config.Tokenizer();
+	public static class MarkovBuilder
+	{
+		public static Markov Build(MarkovConfig config)
+		{
+			List<string> tokens = config.Tokenizer();
 
-            var model = new Dictionary<string, Result>();
+			var model = new Dictionary<string, Result>();
 
-            string prev = null;
+			string prev = null;
 
-            for (int i = 0; i < tokens.Count; i++)
-            {
-                string token = tokens[i];
+			for (int i = 0; i < tokens.Count; i++)
+			{
+				string token = tokens[i];
 
-                if (model.ContainsKey(token))
-                {
-                    model[token].Counter++;
+				if (model.ContainsKey(token))
+				{
+					model[token].Counter++;
 
-                    if (model[prev].NextToken.ContainsKey(token))
-                    {
-                        model[prev].NextToken[token]++;
-                    }
-                    else
-                    {
-                        model[prev].NextToken.Add(token, 1);
-                    }
-                }
-                else
-                {
-                    if (prev == null)
-                    {
-                        model.Add(token, new Result(token));
-                    }
-                    else
-                    {
-                        model.Add(token, new Result(token));
+					if (model[prev].NextToken.ContainsKey(token))
+					{
+						model[prev].NextToken[token]++;
+					}
+					else
+					{
+						model[prev].NextToken.Add(token, 1);
+					}
+				}
+				else
+				{
+					if (prev == null)
+					{
+						model.Add(token, new Result(token));
+					}
+					else
+					{
+						model.Add(token, new Result(token));
 
-                        if (model[prev].NextToken.ContainsKey(token))
-                        {
-                            model[prev].NextToken[token]++;
-                        }
-                        else
-                        {
-                            model[prev].NextToken.Add(token, 1);
-                        }
-                    }
-                }
+						if (model[prev].NextToken.ContainsKey(token))
+						{
+							model[prev].NextToken[token]++;
+						}
+						else
+						{
+							model[prev].NextToken.Add(token, 1);
+						}
+					}
+				}
 
-                prev = token;
-            }
+				prev = token;
+			}
 
-            var markov = new Markov(model);
-            return markov;
-        }
-    }
+			var markov = new Markov(model);
+			return markov;
+		}
+	}
 
-    public class Result
-    {
-        public string Value { get; set; }
+	public class Result
+	{
+		public string Value { get; set; }
 
-        public bool IsStartingWord => Regex.IsMatch(Value, @"[.!?]");
+		public bool IsStartingWord => Regex.IsMatch(Value, @"[.!?]");
 
 
-        public int Counter { get; set; } = 1;
-        public Dictionary<string, int> NextToken { get; set; } = new Dictionary<string, int>();
+		public int Counter { get; set; } = 1;
+		public Dictionary<string, int> NextToken { get; set; } = new Dictionary<string, int>();
 
-        public Result(string value)
-        {
-            Value = value;
-        }
+		public Result(string value)
+		{
+			Value = value;
+		}
 
-        public string Random => Selector.SelectRandomItem();
+		public string Random => Selector.SelectRandomItem();
 
-        private DynamicRandomSelector<string> _selector = null;
-        private DynamicRandomSelector<string> Selector
-        {
-            // not thread safe (yet)
-            get
-            {
-                if (_selector == null)
-                {
-                    _selector = new DynamicRandomSelector<string>();
+		private DynamicRandomSelector<string> _selector = null;
+		private DynamicRandomSelector<string> Selector
+		{
+			// not thread safe (yet)
+			get
+			{
+				if (_selector == null)
+				{
+					_selector = new DynamicRandomSelector<string>();
 
-                    foreach (KeyValuePair<string, int> record in NextToken)
-                    {
-                        float raw = (float)record.Value / (float)Counter;
+					foreach (KeyValuePair<string, int> record in NextToken)
+					{
+						float raw = (float)record.Value / (float)Counter;
 
-                        _selector.Add(record.Key, raw);
-                    }
+						_selector.Add(record.Key, raw);
+					}
 
-                    _selector.Build();
-                }
+					_selector.Build();
+				}
 
-                return _selector;
-            }
-        }
-    }
+				return _selector;
+			}
+		}
+	}
 }
